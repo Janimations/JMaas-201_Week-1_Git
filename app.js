07d7254f79649bf37ccd27bfec2272277f682dcf
Janimations <janimations@gmx.de>
2016-03-15 16:56:57 -0700

    first Lab assignment - first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+// 201
+// JMaas-Week1 Tuesday Lab ASSIGNMENT
+
+// validate user input
+
+//01 Where - Route
+var climbingRoute = prompt('Which Mountain are you planning to climb? Mt.StHellens, Mt.Hood or Mt.Rainier?');
+console.log('Sweet! You are going to climbing' + climbingRoute + '! Good choice.');
+
+// add variables that differentiate between StHellens, Hood and Rainer
+
+//02 Who with ?
+// confirm experience
+
+var climbingPartner = prompt('Who are You climbing with?');
+console.log('Great, so You are climbing with ' + climbingPartner + '.');
+
+//03 Conditions
+// add resource links... or iframes ?
+
+
+//04 Research
+
+//05 Gear
+
+//06 Register
+
+
+
+
+alert('So, ' + userName + ', I heard you like ' + userGame);
+
+var loveHate = confirm(userName + ' do you love interweb?');
+alert('Does, ' + userName + ' love interweb? ' + loveHate);
