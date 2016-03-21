6175693f027b684b1422cb9d862a822da9f84b6d
Janimations <janimations@gmx.de>
2016-03-20 23:14:36 -0700

    Sun 11:10pm JS build done, but debugging problems. Use files index_04.html, game.html, app_04.js and stylesheet.css

--- app_04.js
+++ app_04.js
@@ -19,6 +19,8 @@ questionsArray[4] = How old is Pingu? (number input: too-high and too-low, max 5
 questionsArray[5] = Name Pingu's family members (multiple correct answers)
 ************************************/
 
+// The Idea is to have an array with objects, who themselves store information in properties that are arrays... 2 levels deep:
+
 
 var questionsArray = [
 
@@ -29,47 +31,47 @@ var questionsArray = [
         // Question 01: Made with CG-Animation?
         {
             promptUser: 'Alright ' + userName + ' ,This is a difficult one: Was Pingu animated in CG-Animation or with puppets?  Please type only one single word as your answer:',
-            checkAnswer = ["XXX", "p", "c"],
+            checkAnswer: ["XXX", "p", "c"],
             answerUserTrue: 'Yes! Pingu was made using traditional Stop-Motion!',
-            answerUserFalse = ['No, sorry, all the puppets and sets in Pingu are handmade!', 'Sorry, I didn\'t understand your answer. Please try again and only one word:' ],
-            correctAnswersArray = [],
-            consoleLog = ['--- Question 01 correct answer ---', '--- Question 01 wrong answer ---'],
+            answerUserFalse: ['No, sorry, all the puppets and sets in Pingu are handmade!', 'Sorry, I didn\'t understand your answer. Please try again and only one word:' ],
+            correctAnswersArray: [],
+            consoleLog: ['--- Question 01 correct answer ---', '--- Question 01 wrong answer ---'],
         },
         // Question 02:  Born at South-Pole?
         {
             promptUser: 'So tell me ' + userName + ' , was Pingu born at the North-Pole?  Please answer "Yes" or "No":',
-            checkAnswer = ["XXX", "y", "n"],
+            checkAnswer: ["XXX", "y", "n"],
             answerUserTrue: 'YES! That is correct. Penguins are only found at the South-Pole.',
-            answerUserFalse = ['No! Penguins only live at the South-Pole!', "Sorry, I didn't understand your answer. Please try again and only type 'Yes' or 'No':"],
-            correctAnswersArray = [],
-            consoleLog = ['--- Question 02 correct answer ---', '--- Question 02 wrong answer ---'],
+            answerUserFalse: ['No! Penguins only live at the South-Pole!', "Sorry, I didn't understand your answer. Please try again and only type 'Yes' or 'No':"],
+            correctAnswersArray: [],
+            consoleLog: ['--- Question 02 correct answer ---', '--- Question 02 wrong answer ---'],
         },
         // Question 03: Is Pinga Pigu's sister?
         {
             promptUser: 'Is Pinga Pingu\'s sister? Please answer "Yes" or "No":',
-            checkAnswer = ["XXXX", "y", "n"],
+            checkAnswer: ["XXXX", "y", "n"],
             answerUserTrue: 'Yes ! That is Correct!',
-            answerUserFalse = ['WRONG! she really is his sister!', "Sorry, I didn't understand your answer. Please try again and only type 'Yes' or 'No':"],
-            correctAnswersArray = [],
-            consoleLog = ['--- Question 03 correct answer ---', '--- Question 03 wrong answer ---'],
+            answerUserFalse: ['WRONG! she really is his sister!', "Sorry, I didn't understand your answer. Please try again and only type 'Yes' or 'No':"],
+            correctAnswersArray: [],
+            consoleLog: ['--- Question 03 correct answer ---', '--- Question 03 wrong answer ---'],
         },
         // Question 04:  How old is Pingu?    // Numeric input, too-high too-low + limit to 5 guesses:
         {
             promptUser: 'Take a guess: How old is Pingu? (You can guess 3 times)',
-            checkAnswer = [5],
+            checkAnswer: [5],
             answerUserTrue: 'Well Done! You guessed right!',
-            answerUserFalse = ['No he is older than that, and he already goes to school. Try again:', "No, that's too old. Try again:", "Sorry, I didn't understand your answer. Please try again and only type a number:"],
-            correctAnswersArray = [0, 0],  // index has two items already so that it inly takes three more guesses to break the while loop
-            consoleLog = ['--- Guess number: ' + correctAnswersArray.length + ' --- UserResponse: ' + UserResponse + ' ---'],
+            answerUserFalse: ['No he is older than that, and he already goes to school. Try again:', "No, that's too old. Try again:", "Sorry, I didn't understand your answer. Please try again and only type a number:"],
+            correctAnswersArray: [0, 0],  // index has two items already so that it inly takes three more guesses to break the while loop
+            consoleLog: ['--- Guess number: ' + questionsArray[i].correctAnswersArray.length + ' --- UserResponse: ' + UserResponse + ' ---'],
         },
         // Question 05:
         {
             promptUser: "Name as many of Pingu's family members as you can, separated by a comma:",
-            checkAnswer = ["XXXX", "father", "mother", "pingu", "pinga", "grandfather"],
-            answerUserTrue: "You guessed " + correctAnswersArray.length + " of Pingu's family members correctly. - " + correctAnswersArray,
-            answerUserFalse = ["Sorry, but none of the persons you entered are part of Pingu's family...", "Would You like to guess again?", 'Sorry, I did not understand your answer. Try again: Name as many of Pingu\'s family members as you can, separated by a comma:'],
-            correctAnswersArray = [],
-            consoleLog = ['--- error, user Input not a string ---'],
+            checkAnswer: ["XXXX", "father", "mother", "pingu", "pinga", "grandfather"],
+            answerUserTrue: "You guessed " + questionsArray[i].correctAnswersArray.length + " out of 5 of Pingu's family members correctly. - " + correctAnswersArray,
+            answerUserFalse: ["Sorry, but none of the persons you entered are part of Pingu's family... Would You like to guess again?", 'Sorry, I did not understand your answer. Try again: Name as many of Pingu\'s family members as you can, separated by a comma:'],
+            correctAnswersArray: [],
+            consoleLog: ['--- Input error, " ' + userResponseArray[x] + ' " is not a string ---'],
         },
 
 ];          // questionsArray close
@@ -100,9 +102,10 @@ for (var i = 1; i < questionsArray.length; i++) {                               
         var userResponse = prompt(questionsArray.promptUser[i]);                // gets string from promptUser property in questionsArray
         var userResponseArray = userResponse.split(", ");                       // userResponse is now an Array ! this is for all questions !
 
+
+        // Questions 01, 02, 03: Yes/No or p/c:
         if (userResponseArray.length < 2 && parseInt(userResponseArray[0]) == NaN) {      // if the userResponseArray contains only one answerUserResponse (Question 05 has multiple) and contained a "word-string" parseInt(userResponseArray[0]) will return Nan, and this will run
 
-            // Questions 01, 02, 03:  Yes/No and c/p questions:
             userResponseSlice = userResponseArray[0].slice(0, 1);  //slices of the first character
 
             if (userResponseSlice.toLowerCase() === questionsArray[i].checkAnswer[1]) {
@@ -126,7 +129,7 @@ for (var i = 1; i < questionsArray.length; i++) {                               
 
 
         // Question 04: numeric input, too-high too-low + limit to 3 guesses:
-        } else if (userResponseArray.length < 2 && typeof userResponseArray[0] === 'number') {
+        } else if (userResponseArray.length < 2 && typeof userResponseArray[0] === 'number') {      // if its only one entry and a number it "shuold" be for question 04
             if (parseInt(userResponseArray[0]) == 5) {
                 alert(questionsArray[i].answerUserTrue);
                 question_4.textContent = questionsArray[i].answerUserTrue;
@@ -146,6 +149,7 @@ for (var i = 1; i < questionsArray.length; i++) {                               
             } else {
               alert(questionsArray[i].answerUserFalse[2]);                                  // error message, does not count as a guess
               console.log("---- Question 04 User Input error -----");
+            };
 
         // Question 5: multiple correct answers
         } else {
@@ -153,16 +157,24 @@ for (var i = 1; i < questionsArray.length; i++) {                               
                 for (var x = 0; x < userResponseArray.length; x++) {                    // x loops through userResponseArray
 
                     if (questionsArray[i].checkAnswer.indexOf(userResponseArray[x].toLowerCase()) > 0) {       // if item is in the checkAnswer array indexOf will return a number > 0
-                        questionsArray[i].correctAnswersArray.push(userResponseArray[x]                         // item gets pushed into correctAnswersArray... (+1)
+                        questionsArray[i].correctAnswersArray.push(userResponseArray[x]);                         // item gets pushed into correctAnswersArray... (+1)
 
-
+                    // errors:
                     } else if (userResponseArray[x] !== 'string') {                         // error msg if input is not a string
                         console.log(questionsArray[i].consoleLog[0]);
+                    } else if (questionsArray[i].correctAnswersArray.length == 0) {
+                        console.log(questionsArray[i].answerUserFalse[0]);
+                        alert(questionsArray[i].answerUserFalse[0]);
+                    } else {
+                        console.log(questionsArray[i].answerUserFalse[1]);
+                        alert(questionsArray[i].answerUserFalse[1]);
                     };
                 };   // 05 for-loop close
             };   // 05 while-loop close
 
             // User Feedback: how many correct out of 5:
+            console.log(questionsArray[i].answerUserTrue);
+
 
 
         }; // else close
@@ -173,8 +185,8 @@ for (var i = 1; i < questionsArray.length; i++) {                               
 
 // Feedback to User:
 
-
     // total score:
+
 
 
 
